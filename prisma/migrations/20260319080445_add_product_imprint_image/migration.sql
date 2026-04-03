0389fdc545c57fcb04828691e4697dca5c528825
Matvej911 <matvejaleksejv@gmail.com>
2026-04-03 20:55:38 +0300

    to big case

--- prisma/migrations/20260319080445_add_product_imprint_image/migration.sql
+++ prisma/migrations/20260319080445_add_product_imprint_image/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `product` ADD COLUMN `imprintImage` VARCHAR(191) NULL;
+ALTER TABLE `Product` ADD COLUMN `imprintImage` VARCHAR(191) NULL;
--- prisma/migrations/20260324181935_add_featured_products/migration.sql
+++ prisma/migrations/20260324181935_add_featured_products/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `product` ADD COLUMN `featured` BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE `Product` ADD COLUMN `featured` BOOLEAN NOT NULL DEFAULT false;
